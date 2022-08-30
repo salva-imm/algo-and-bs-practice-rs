63b58a57990c5561150936560bada5ae2040c7e3
salva-imm <mehdi73ee@gmail.com>
2022-08-31 03:06:56 +0430

    defined struct

---
+++ c-lab/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct {
+    char first;
+    char second;
+    char third;
+    short fourth;
+} sample_t;
+void print_size(sample_t* var) {
+    printf("Size: %lu bytes\n", sizeof(*var));
+}
+void print_bytes(sample_t* var) {
+    unsigned char* ptr = (unsigned char*)var;
+    for (int i = 0; i < sizeof(*var); i++, ptr++) {
+        printf("%c ", (char )*ptr);
+    }
+    printf("\n");
+}
+int main(int argc, char** argv) {
+    sample_t var = {'A', 'B', 'C', 765 };
+    print_size(&var);
+    print_bytes(&var);
+    return 0;
+}
